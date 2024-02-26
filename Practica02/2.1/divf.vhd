389007e5c91637aa3a06ee7e39f1c5df63ee0f59
Franco Ruiz <54223581+Franco0932@users.noreply.github.com>
2024-02-26 10:20:35 -0600

    Update divf.vhd

--- Practica02/2.1/divf.vhd
+++ Practica02/2.1/divf.vhd
@@ -19,7 +19,7 @@ begin
 			if (clkl' event and clkl='1') then --ta un flanco de subida en el reloj de la tarjeta, se procede a la condicional siguiente.
 				if (contador = 25000000) then --Si el contador llega a 25000000, se regresa a 0 y se invierte el valor de clkl.
 					contador <= 0;
-					led     <= not clkl;
+					led     <= not (led);
 				else
 					contador <= contador + 1; --Cada flanco de subida se le suma uno al contador.
 				end if;
