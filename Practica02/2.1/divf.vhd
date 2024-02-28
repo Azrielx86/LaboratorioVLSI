b41798709510f55f6468d6fe278b7f8d72494d09
Franco Ruiz <54223581+Franco0932@users.noreply.github.com>
2024-02-28 09:55:55 -0600

    Update divf.vhd

--- Practica02/2.1/divf.vhd
+++ Practica02/2.1/divf.vhd
@@ -5,8 +5,8 @@ use ieee.std_logic_1164.all;
 entity relojlento is
 port
 	(
-	clkl  : in std_logic; 
-	led : buffer std_logic:='0' 
+	clkl  : in std_logic; --Entrada del Reloj
+	led : buffer std_logic:='0' --Salida del LED
 	);
 end entity;
 
@@ -18,8 +18,8 @@ begin
 		begin
 			if (clkl' event and clkl='1') then --ta un flanco de subida en el reloj de la tarjeta, se procede a la condicional siguiente.
 				if (contador = 25000000) then --Si el contador llega a 25000000, se regresa a 0 y se invierte el valor de clkl.
-					contador <= 0;
-					led     <= not (led);
+					contador <= 0;  
+					led     <= not (led); --Cambia el estado del LED
 				else
 					contador <= contador + 1; --Cada flanco de subida se le suma uno al contador.
 				end if;
